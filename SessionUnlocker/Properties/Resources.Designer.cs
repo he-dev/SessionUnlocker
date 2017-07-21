9c1eab6d8bf33effbe2cd0af35b5fcc8fe59b430
MTk4MQ <bennyboss+github@gmail.com>
2017-07-21 22:23:00 +0200

    - added icon

--- SessionUnlocker/Properties/Resources.Designer.cs
+++ SessionUnlocker/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace SessionUnlocker.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon software_development {
+            get {
+                object obj = ResourceManager.GetObject("software_development", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
